16387f10148fca2074be79be949c8e9b1d18905f
ReidGibson-Bingham <jsr3b0rn@gmail.com>
2023-11-21 23:49:18 -0800

    created the Hero folder, file and styles file.

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// need to import the hero pic data. refer to one of the videos from day 2. I beleive it is one that mark made. 
+
+
+const Hero = () => {
+
+    return (
+        <img className="hero" src=""></img>
+    )
+
+}
+
+export default Hero;
